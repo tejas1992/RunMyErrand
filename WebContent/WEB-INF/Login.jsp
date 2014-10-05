1e997dbc9e7bef669a70986e0ad47f3ab3887d53
abhivija <abhivija@indiana.edu>
2014-10-05 17:44:23 -0400

    Skeleton of Web App with Login/Signup page

---
+++ WebContent/WEB-INF/Login.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<h1>First Spring MVC Application Demo</h1>
+ 
+	<h2>${welcomeMessage}</h2>
+ 
+</body>
+</html>
---
+++ src/com/runMyErrand/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.runMyErrand.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class LoginController{
+
+	@RequestMapping("/")
+	public ModelAndView login()
+	{
+		ModelAndView model = new ModelAndView("signin");		
+		return model;
+		
+	}
+
+}
